e036659b19bb61bba4893ba7cab38a3ef6b23e57
zh <2745524959@qq.com>
2025-07-17 17:43:37 +0800

    feat日志todo

--- api/enter.go
+++ api/enter.go
@@ -1,9 +1,53 @@
-package api
+// api/site_api/enter.go
+package site_api
 
-import "blogx_server/api/site_api"
+import (
+	"blogx_server/models/enum"
+	"blogx_server/service/log_service"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"time"
+)
 
-type Api struct {
-	SiteApi site_api.SiteApi
+type SiteApi struct {
 }
 
-var App = Api{}
+func (SiteApi) SiteInfoView(c *gin.Context) {
+	log_service.NewLoginSuccess(c, enum.UserPwdLoginType)
+	log_service.NewLoginFail(c, enum.UserPwdLoginType, "用户不存在", "fengfeng", "1234")
+	c.JSON(200, gin.H{"code": 0, "msg": "站点信息"})
+	return
+}
+
+type SiteUpdateRequest struct {
+	Name string `json:"name" binding:"required"`
+}
+
+func (SiteApi) SiteUpdateView(c *gin.Context) {
+	log := log_service.GetLog(c)
+
+	log.ShowRequest()
+	log.ShowRequestHeader()
+	log.ShowResponseHeader()
+	log.ShowResponse()
+	log.SetTitle("更新站点")
+	log.SetItemInfo("请求时间", time.Now())
+	log.SetImage("/xxx/xxx")
+	log.SetLink("yaml学习地址", "https://www.fengfengzhidao.com")
+	c.Header("xxx", "xxee")
+	var cr SiteUpdateRequest
+	err := c.ShouldBindJSON(&cr)
+	if err != nil {
+		log.SetError("参数绑定失败", err)
+	}
+	log.SetItemInfo("结构体", cr)
+	log.SetItemInfo("切片", []string{"a", "b"})
+	log.SetItemInfo("字符串", "你好")
+	log.SetItemInfo("数字", 123)
+	id := log.Save()
+	fmt.Println(1, id)
+	id = log.Save()
+	fmt.Println(2, id)
+	c.JSON(200, gin.H{"code": 0, "msg": "站点信息", "data": 1})
+	return
+}
--- router/enter.go
+++ router/enter.go
@@ -1,14 +1,20 @@
+// router/enter.go
 package router
 
 import (
 	"blogx_server/global"
+	"blogx_server/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func Run() {
 	r := gin.Default()
+
 	nr := r.Group("/api")
+
+	nr.Use(middleware.LogMiddleware)
 	SiteRouter(nr)
+
 	addr := global.Config.System.Addr()
 	r.Run(addr)
 }
